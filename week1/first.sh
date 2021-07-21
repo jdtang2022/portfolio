0a9aed1558d8e8cfd21c975e91e9180c0df587a4
jdtang2022 <jdtang@our.ecu.edu.au>
2021-07-21 02:18:07 +0000

    Week 1 Scripts with comments added

--- week1/first.sh
+++ week1/first.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+#
+# firsh.sh
+# @version 1.0
+# A simple script to print out text
+#
+# @author Jay <jdtang@our.ecu.edu.au>
+# @date: 14/07/2021
+
+# print "Hello"
 echo "Hello"
+
+# print "This is awesome"
 echo "This is awesome!"
+
 exit 0
--- week1/second.sh
+++ week1/second.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+#
+# second.sh
+# @version 1.0
+# Script receives input from the command line while executed.
+# Each argument typed into the command line is referred to by a special numeric variable "$1".
+# Execute this code by providing the value for $1 by typing into the command line after the path to the script.
+#	eg.	./second.sh Jay
+#
+# @author Jay <jdtang@our.ecu.edu.au>
+# @date: 14/07/2021
 
+# print 'Hi there' message
 echo "Hi there!"
 
+# argument $1 receive input from the command line, then print out with the message
 echo "It's good to see you $1!"
 
+# exit with success
 exit 0
