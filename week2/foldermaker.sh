0eef2cd14fdc880c02139f51fb0dfa795fb6d38a
jdtang2022 <jdtang@our.ecu.edu.au>
2021-08-17 03:04:00 +0000

    Add Week 2 scripts to portfolio

--- week2/foldermaker.sh
+++ week2/foldermaker.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
-# foldermaker.sh
-# @version 1.0
-# This script is for creating folder by taking folder name from user input in command line.
+# Filename: foldermaker.sh
+# @version: 1.0
+# Description: This script is for creating folder by taking folder name from user input in command line.
 #
-# @author Jay <jdtang@our.ecu.edu.au>
-# @date: 21/07/2021
+# @author: Jay <jdtang@our.ecu.edu.au>
+# @creation date:      21/02/2021
+# @last modified date: 17/08/2021
 
 # use read command to take input from user and store in 'folderName' variable
 read -p "Type the name of the folder you would like to create: " folderName
