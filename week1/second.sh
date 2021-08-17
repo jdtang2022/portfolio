88c7bb9fd74071ff1b1809600ac898dde093efd6
jdtang2022 <jdtang@our.ecu.edu.au>
2021-08-17 02:50:36 +0000

    Add Week 1 scripts to portfolio

--- week1/second.sh
+++ week1/second.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #
-# second.sh
-# @version 1.0
-# Script receives input from the command line while executed.
-# Each argument typed into the command line is referred to by a special numeric variable "$1".
-# Execute this code by providing the value for $1 by typing into the command line after the path to the script.
-#	eg.	./second.sh Jay
+# Filename: second.sh
+# @version: 1.0
+# Description: Script receives input from the command line while executed.
+#               Each argument typed into the command line is referred to by a special numeric variable "$1".
+#               Execute this code by providing the value for $1 by typing into the command line after the path to the script.
+#               	eg.	./second.sh Jay
 #
-# @author Jay <jdtang@our.ecu.edu.au>
-# @date: 14/07/2021
+# @author: Jay <jdtang@our.ecu.edu.au>
+# @creation date:      14/07/2021
+# @last modified date: 17/08/2021
 
 # print 'Hi there' message
 echo "Hi there!"
